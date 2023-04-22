910420535cceb654a5a44e9568bce6ec16d474b0
S.H.YE <31349568+HyuChung@users.noreply.github.com>
2023-04-23 08:09:15 +0900

    fin chap14+

--- UndeadZombie/Assets/Scripts/AchiveManager.cs
+++ UndeadZombie/Assets/Scripts/AchiveManager.cs
@@ -7,13 +7,17 @@ public class AchiveManager : MonoBehaviour
 {
     public GameObject[] lockCharacter;
     public GameObject[] unlockCharacter;
+    public GameObject uiNotice;
     
     enum Achive {UnlockPotato, UnlockBean }
     Achive[] achives;
 
+    WaitForSecondsRealtime wait;
+
     void Awake()
     {
         achives = (Achive[])Enum.GetValues(typeof(Achive));
+        wait = new WaitForSecondsRealtime(5);
 
         if(!PlayerPrefs.HasKey("MyData")){
             Init();
@@ -26,7 +30,7 @@ public class AchiveManager : MonoBehaviour
 
         foreach (Achive achive in achives)
         {
-            PlayerPrefs.SetInt(achive.ToString(), 1);
+            PlayerPrefs.SetInt(achive.ToString(), 0);
         }
     }
 
@@ -48,8 +52,45 @@ public class AchiveManager : MonoBehaviour
     
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        foreach(Achive achive in achives){
+            CheckAchive(achive);
+        }
+    }
+
+
+    void CheckAchive(Achive achive)
+    {
+        bool isAchive = false;
+
+        switch(achive){
+            case Achive.UnlockPotato:
+                isAchive = GameManager.instance.Kill >= 10;
+                break;
+            case Achive.UnlockBean:
+                isAchive = GameManager.instance.GameTime == GameManager.instance.MaxGameTime;
+                break;
+        }
+
+        if(isAchive && PlayerPrefs.GetInt(achive.ToString())==0 ){
+            PlayerPrefs.SetInt(achive.ToString(), 1);
+
+            for(int index = 0; index< uiNotice.transform.childCount; index++){
+                bool isActive = index == (int)achive;
+                uiNotice.transform.GetChild(index).gameObject.SetActive(isActive);
+            }
+
+            StartCoroutine(NoticeRoutine());
+        }
+    }
+
+    IEnumerator NoticeRoutine()
+    {   
+        uiNotice.SetActive(true);
+
+        yield return wait;
+
+        uiNotice.SetActive(false);
     }
 }
